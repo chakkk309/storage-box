af65e89460b33e98bdd91d9d529d203047fede94
cha-cha369 <1301826221@qq.com>
2021-10-23 22:32:28 +0800

    update README.md

--- backend/src/main/java/com/ruoyi/project/storage/controller/AppAddressController.java
+++ backend/src/main/java/com/ruoyi/project/storage/controller/AppAddressController.java
@@ -14,9 +14,6 @@ import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
